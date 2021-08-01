0c5427590dd85616731c349f8b717fb9b46b04b1
Liam Chipman <chipman.liam@gmail.com>
2021-07-31 21:50:57 -0300

    Moved the API key to an external file
    made the api key be read from an external file to avoid fraud issues

--- Main.py
+++ Main.py
@@ -2,7 +2,9 @@ from RiotAPI import RiotAPI
 import RiotConsts as Consts
 
 def main():
-    api = RiotAPI('RGAPI-16de9793-ce12-43f6-85b7-bf5b84be4f8d')
+    api_file = open("../../API_Key.txt", 'r')
+    api = RiotAPI(api_file.read())
+    api_file.close()
     r = api.getSummonerByName('Legonerd666', Consts.REGIONS['north_america'])
     print(r)
     r2 = api.getMatchIdByPuuid(r['puuid'], Consts.REGIONS['americas'], 0, 1)
